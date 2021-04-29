23a2dfd8b177035a2eef50d30cb843cc476574a8
morgancostigan <morgan.costigan@gmail.com>
2021-04-29 12:13:13 -0500

    added readmes and index.js

---
+++ frontend/README.md
@@ -0,0 +1,8 @@
+Sick Fits Frontend
+
+# React
+# Next.js
+# Apollo Client
+# Styled Components
+
+
---
+++ frontend/pages/index.js
@@ -0,0 +1,5 @@
+export default function index() {
+    return <div>
+        <p>Kia Ora!</p>
+    </div>
+}
