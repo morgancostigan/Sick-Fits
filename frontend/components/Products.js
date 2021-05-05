3e424ec12c9fc3575dc8ba5adbf342182ee8f72d
morgancostigan <morgan.costigan@gmail.com>
2021-05-05 14:01:07 -0500

    wrote ALL_PRODUCTS_QUERY gql and using it with useQuery hook

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,4 +1,25 @@
+import { useQuery } from '@apollo/client';
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+    query ALL_PRODUCTS_QUERY {
+        allProducts {
+            id
+            name
+            price
+            description
+            photo {
+                id
+                image {
+                    publicUrlTransformed
+                }
+            }
+        }
+    }
+`;
+
 export default function Products() {
+    useQuery(ALL_PRODUCTS_QUERY);
     return (
         <div>
             <p>Productos</p>
