6e4c614f7fb41b04d14b50f9481e406b3a575031
morgancostigan <morgan.costigan@gmail.com>
2021-05-06 13:44:35 -0500

    in the useMutation for CREATE_PRODUCT_MUTATION refetchQueries fires ALL_PRODUCTS_QUERY so new items are immediately available on the products page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "010287b2de773f7eb180";
+/******/ 	var hotCurrentHash = "03179a4f6683e6050a58";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/CreateProduct.js
+++ frontend/components/CreateProduct.js
@@ -2,6 +2,7 @@ import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import useForm from "../lib/useForm";
 import DisplayError from './ErrorMessage';
+import { ALL_PRODUCTS_QUERY } from "./Products";
 import Form from './styles/Form';
 
 const CREATE_PRODUCT_MUTATION = gql`
@@ -42,14 +43,15 @@ export default function CreateProduct() {
     });
     const [createProduct, {loading, error, data}] = useMutation(CREATE_PRODUCT_MUTATION, {
         variables: inputs,
+        refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
     });
     
     return (
         <Form onSubmit={ async (e) => {
             e.preventDefault();
-            console.log({inputs});
+            // console.log({inputs});
             //submit input fields to backend
-            const res = await createProduct();
+            await createProduct();
             clearForm();
         }}>
             <DisplayError error={error}/>
