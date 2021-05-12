174248912db348e6024593f12764caca58780d39
morgancostigan <morgan.costigan@gmail.com>
2021-05-12 13:16:14 -0500

    added useRouter hook to products/index to access the queried page number

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e8437b2c7244c196075c";
+/******/ 	var hotCurrentHash = "400403d876586ae0d872";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/products/index.js
+++ frontend/pages/products/index.js
@@ -1,12 +1,15 @@
 import Products from '../../components/Products';
 import Pagination from '../../components/Pagination';
+import { useRouter } from 'next/router';
 
 export default function ProductPage() {
+    const { query  } = useRouter();
+    
     return (
         <div>
-            <Pagination page={1}/>
+            <Pagination page={query.page || 1}/>
             <Products />
-            <Pagination page={1}/>
+            <Pagination page={query.page || 1}/>
         </div>
     );
 };
