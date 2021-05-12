00b3f40501fdc0104f5406d5b1ed02f5ac9766de
morgancostigan <morgan.costigan@gmail.com>
2021-05-11 19:38:32 -0500

    imported useMutation hook into DeleteProduct.js and passed it DELETE_PRODUCT_MUTATION and the id, followed by a catch to show any errors in an alert

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "61d4df14837bf990136d";
+/******/ 	var hotCurrentHash = "ba8e0fc923218b239539";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -1,3 +1,4 @@
+import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 
 const DELETE_PRODUCT_MUTATION = gql`
@@ -10,11 +11,16 @@ const DELETE_PRODUCT_MUTATION = gql`
 `;
 
 export default function DeleteProduct({id, children}) {
+    const [deleteProduct, {loading}] = useMutation(
+        DELETE_PRODUCT_MUTATION, {
+            variables: { id },
+        }
+    );
     return <button type="button" onClick={() => {
         if(confirm('Should we really remove this item from existence?')) {
             //if confirmed, THEN delete
             console.log('gettin it gone!');
-            
+            deleteProduct().catch(err => alert(err.message));
         }
     }}>
         {children}
