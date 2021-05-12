ed58e65ca9a2c71f8b24cc5ec28a61f0d4322098
morgancostigan <morgan.costigan@gmail.com>
2021-05-11 19:50:41 -0500

    after deleting, we now run an update function that does a cache.evict from the Apollo cache, triggering a react rerender without the deleted product

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "97d8052644962ff8c2da";
+/******/ 	var hotCurrentHash = "a87fec1b8f0be35e43f2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/DeleteProduct.js
+++ frontend/components/DeleteProduct.js
@@ -10,10 +10,19 @@ const DELETE_PRODUCT_MUTATION = gql`
     }
 `;
 
+function update(cache, payload) {
+    console.log({payload});
+    console.log('Running update fnction after Delete.');
+    //remove payload item from Apollo cache
+    cache.evict(cache.identify(payload.data.deleteProduct))
+    
+}
+
 export default function DeleteProduct({id, children}) {
     const [deleteProduct, {loading}] = useMutation(
         DELETE_PRODUCT_MUTATION, {
             variables: { id },
+            update: update,
         }
     );
     return <button 
