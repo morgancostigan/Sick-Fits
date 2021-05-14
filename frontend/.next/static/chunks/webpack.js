1c698b8145b358e1383f615ed06c676e9c74fcf8
morgancostigan <morgan.costigan@gmail.com>
2021-05-14 14:01:58 -0500

    began writing Sign In form.  used useForm to set initial const values

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a249782750606ffe997c";
+/******/ 	var hotCurrentHash = "aacf713162ca974c0922";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/SignIn.js
+++ frontend/components/SignIn.js
@@ -1,8 +1,22 @@
 import Form from './styles/Form';
+import useForm from "../lib/useForm";
+import { useQuery } from '@apollo/client';
+
 
 export default function SignIn() {
+
+    const { inputs, handleChange, clearForm, resetForm } = useForm({
+        email: '',
+        password: '',
+    });
+    const [createProduct, { loading, error, data }] = useQuery(CREATE_PRODUCT_MUTATION, {
+        variables: inputs,
+        refetchQueries: [{ query: ALL_PRODUCTS_QUERY }],
+    });
+
     return (
-        <Form onSubmit={async (e) => {
+        //method POST to prevent password from showing in URL, history, and logs
+        <Form method="post" onSubmit={async (e) => {
             e.preventDefault();
             // console.log({inputs});
             //submit input fields to backend
@@ -22,7 +36,8 @@ export default function SignIn() {
                         type="email"
                         id="email"
                         name="email"
-                        placeholder="email"
+                        placeholder="Please Enter Your Email"
+                        autoComplete="email"
                         value={inputs.email}
                         onChange={handleChange}
                     />
@@ -33,7 +48,8 @@ export default function SignIn() {
                         type="password"
                         id="password"
                         name="password"
-                        placeholder="password"
+                        placeholder="..And Your Password"
+                        autoComplete="password"
                         value={inputs.password}
                         onChange={handleChange}
                     />
