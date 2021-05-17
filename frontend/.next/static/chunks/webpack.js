7fbcda85e3476a9a795201e57df961295f090398
morgancostigan <morgan.costigan@gmail.com>
2021-05-17 13:55:33 -0500

    added Sign Out button to SignOut.js

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0cd3d06dac7d5d5a9ecb";
+/******/ 	var hotCurrentHash = "f3072424b69b198d8b6a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/SignOut.js
+++ frontend/components/SignOut.js
@@ -4,3 +4,43 @@ import DisplayError from './ErrorMessage';
 import { useMutation, useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 import { CURRENT_USER_QUERY } from './User';
+
+const SIGN_OUT_MUTATION = gql`
+    mutation SIGN_OUT_MUTATION {
+        endSession
+    }
+`;
+
+export default function SignOut({children}){
+    const { inputs, handleChange, clearForm, resetForm } = useForm({
+        email: '',
+        password: '',
+    });
+    const [signout, { data, loading }] = useMutation(SIGN_OUT_MUTATION, {
+        variables: inputs,
+        refetchQueries: [{ query: CURRENT_USER_QUERY }]
+    });
+
+    async function handleSubmit(e) {
+        e.preventDefault(); //stop form from submitting early
+        //send email and password to graphQL API
+        // await signin();
+        //const res for logging only
+        const res = await signout()
+        console.log({ res });
+        resetForm();
+    };
+
+
+    return (
+
+                <button type="submit">Sign Out</button>
+
+    )
+
+
+
+
+
+
+}
