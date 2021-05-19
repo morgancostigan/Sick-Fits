91ae976fb7276f6ecd81e1af5e4463c03bffb6b1
morgancostigan <morgan.costigan@gmail.com>
2021-05-19 18:20:20 -0500

    created CartItem.ts and specified the fields

--- backend/lib/mail.ts
+++ backend/lib/mail.ts
@@ -1,4 +1,4 @@
-import { createTransport } from "nodemailer";
+import { createTransport, getTestMessageUrl } from "nodemailer";
 
 const transport = createTransport({
     host: process.env.MAIL_HOST,
@@ -57,6 +57,8 @@ export async function sendPasswordResetEmail(
             <a href="${process.env.FRONTEND_URL}/reset?token=${resetToken}">Click here to reset.</a>
         `)
     })) as MailResponse;
-    console.log({info});
-    
+    if(process.env.MAIL_USER.includes('ethereal.email')) {
+        console.log(`📧 Message Sent! Preview it at ${getTestMessageUrl(info)}`);
+        
+    }   
 }
---
+++ backend/schemas/CartItem.ts
@@ -0,0 +1,16 @@
+import { integer, relationship, select, text } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+
+export const CartItem = list({
+    fields:{
+        //TODO custom label
+        quantity: integer({
+            defaultValue: 1,
+            isRequired: true
+        }),
+        product: relationship({ ref: 'Product' }),
+        user: relationship({ ref: 'User.cart'}),
+
+
+    },
+});
