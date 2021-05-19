f5318ed826041134d2c0b9949df0aae0edc0f3d5
morgancostigan <morgan.costigan@gmail.com>
2021-05-19 15:32:45 -0500

    changed from a Link to Sign In, to directly importing RequestReset component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "d8d937747135c68c41fd";
+/******/ 	var hotCurrentHash = "53710cf54b9c99ecccb0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/pages/reset.js
+++ frontend/pages/reset.js
@@ -1,17 +1,16 @@
 import Link from 'next/link';
+import RequestReset from '../components/RequestReset';
+
 
 export default function ResetPage({query}) {
     if(!query?.token){
         return (
             <div>
-                <p>Please visit Sign In page to send a password reset toekn to your email.</p>
-                <button type="button">
-                    <Link href="/signin">Sign In</Link>
-                </button>
-                
+                <h2>Please request a reset token.</h2>
+                <RequestReset />
             </div>
         )
-    }
+    };
     return (
         <div>
             <p>Reset Your Password {query.token}</p>
