b9377bc5bb24638a1587ec83c6c98290e7109f47
morgancostigan <morgan.costigan@gmail.com>
2021-05-24 15:25:19 -0500

    continuing to write addToCart, fiurst checking that a user is looged in, then seeing if the item they are trying to add is already in their cart

--- backend/mutations/addToCart.ts
+++ backend/mutations/addToCart.ts
@@ -1,11 +1,23 @@
 import { KeystoneContext } from "@keystone-next/types";
+import { CartItemCreateInput } from '../.keystone/schema-types';
+import { Session } from "../types";
 
-export default function addToCart(
+export default async function addToCart(
     root: any, 
     {productID}: {productID: string},
     context: KeystoneContext,
-){
+): Promise<CartItemCreateInput> {
     console.log('adding to the damn cart!');
+    // query current user for signed in value
+    const sesh = context.session as Session;
+    if(!sesh.itemId){
+        throw new Error('You gotta be logged in to do that!')
+    }
+    // query their cart
+    const allCartItems = await context.lists.CartItem.findMany({
+        where: {user: {id: sesh.itemId}, product: {id: productID}}
+    });
+    // see if item being added is already in cart
+    // if yes, increment.  if no, create new CartItem
     
-
 };
