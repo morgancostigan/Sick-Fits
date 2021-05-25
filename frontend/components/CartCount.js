59c4a063a75739715edb7b89f9b0aabe162fba77
morgancostigan <morgan.costigan@gmail.com>
2021-05-25 12:40:23 -0500

    imported CartCount into Nav with a temporary hardcoded count

--- frontend/components/CartCount.js
+++ frontend/components/CartCount.js
@@ -1,6 +1,12 @@
+import styled from "styled-components"
+
+const Dot = styled.div`
+    background: var(--red);
+    color: white;
 
 
+`;
 
-export default function CartCount(){
-
+export default function CartCount({count}){
+    return <Dot>{count}</Dot>
 }
--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -3,6 +3,7 @@ import NavStyles from '../components/styles/NavStyles';
 import { useUser } from './User';
 import { useCart } from '../lib/cartState';
 import SignOut from '../components/SignOut';
+import CartCount from '../components/CartCount';
 
 
 export default function Nav() {
@@ -21,7 +22,10 @@ export default function Nav() {
                         <Link href="/account">Account</Link>
                         {/* <Link href="/cart">Cart</Link> */}
                         <SignOut />
-                <button type="button" onClick={openCart}>🛒</button>
+                <button type="button" onClick={openCart}>
+                    🛒
+                    <CartCount count={12}/>
+                </button>
             </> )}
             {/* if not signed in */}
             { !user && ( <>
