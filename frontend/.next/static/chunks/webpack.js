94c7ee186b49c22e2e114f9cf4437f5bc84cb3ee
morgancostigan <morgan.costigan@gmail.com>
2021-06-02 14:58:48 -0500

    added an update function to run after the gwql REMOVE_FROM_CART_MUTATION, that evicts the payload id from the local cache

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "7fed5c5f6844a4dc0285";
+/******/ 	var hotCurrentHash = "3ca719fb8bac7273749b";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -6,15 +6,22 @@ const REMOVE_FROM_CART_MUTATION = gql`
     mutation REMOVE_FROM_CART_MUTATION($id: ID!) {
         deleteCartItem(id: $id){
             id
+            # __typename - this comes automatically
         }
     }
 `;
 
+function update(cache, payload) {    
+    cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
     const [removeFromCart, { loading }] = useMutation(
-        REMOVE_FROM_CART_MUTATION, 
-        {variables: {id: id}}
-        )
+        REMOVE_FROM_CART_MUTATION, {
+            variables: {id},
+            //named simply update, because updateCart didn't work.  is this an apollo thing?
+            update
+    });
     return <RemoveButton 
         onClick={removeFromCart}
         disabled={loading}
@@ -22,4 +29,4 @@ export default function RemoveFromCart({ id }) {
         type="button" 
         title="remove this item from cart">
         &times;</RemoveButton>
-}
+};
